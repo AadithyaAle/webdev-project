39ad3a5beb6a2a919df3cf0d5ab31b7e797fc7e1
Ale Aadithya <150817844+AadithyaAle@users.noreply.github.com>
2025-12-06 23:25:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -60,3 +60,27 @@ if(savedQuotes){
     renderQuotes();
 }
 
+const themeToggle = document.getElementById('themeToggle');
+
+function applyTheme(theme){
+    if(theme=='light'){
+        document.body.classList.add('light-mode');
+        themeToggle.textContent = 'Dark mode'
+    }else{
+        document.body.classList.remove("light-mode");
+        themeToggle.textContent = 'Light Mode';
+    }
+}
+
+const savedTheme= localStorage.getItem('theme');
+if(savedTheme){
+    applyTheme(savedTheme);
+} else{
+    applyTheme('dark');
+}
+
+themeToggle.addEventListener('click', ()=>{
+    const newTheme = document.body.classList.contains('light-mode') ? 'dark' : 'light';
+    applyTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+})
